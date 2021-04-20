72d348f94f281f104be7fb874a1efaa7bf75ac85
Audrey Fleming <audreyfleming@gmail.com>
2021-04-20 12:24:18 -0400

    outputting participants

--- blockchain.py
+++ blockchain.py
@@ -8,7 +8,7 @@ genesis_block = {
 blockchain = [genesis_block]
 open_transactions = []
 owner = 'Audge'
-
+participants = {'Audge'}
 
 def get_last_blockchain_value():
     if len(blockchain) < 1:
@@ -23,6 +23,8 @@ def add_transaction(recipient, sender= owner, amount=1.0):
     'amount': amount
     }
     open_transactions.append(transaction)
+    participants.add(sender)
+    participants.add(recipient)
 
 
 
@@ -76,6 +78,7 @@ while waiting_for_input:
     print('1: Add a new transaction value')
     print('2: Mine a new block')
     print('3: Output the blockchain blocks')
+    print('4: Output participants')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
@@ -88,6 +91,8 @@ while waiting_for_input:
         mine_block()
     elif user_choice == '3':
         print_blockchain_elements()
+    elif user_choice == '4':
+    	print(participants)
     elif user_choice == 'h':
         if len(blockchain) >= 1:
             blockchain[0] = {
